0b6c8cd9e131ec0186dab59e05693013283105ce
IronLuaAlyria <68116801+IronLuaAlyria@users.noreply.github.com>
2020-07-10 16:35:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+
+});
+
+client.login('NzI4NjQ2MDU3ODA2MDA0Mzg3.Xwh0Dg.Phov1Q5QUDA7NiDYaTeqxOPol1A');
